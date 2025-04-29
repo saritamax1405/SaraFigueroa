2d4f7073511f18c23ada727d0b4cfbe2645ba5b2
saritamax1405 <sfigueroa1405@gmail.com>
2025-04-29 11:11:26 -0500

    PWA
    PWA

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
+            src: 'icons/perro.png',
+            sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
+            src: 'icons/perro2.png',
+            sizes: '128x128',
             type: 'image/png'
           }
         ]
